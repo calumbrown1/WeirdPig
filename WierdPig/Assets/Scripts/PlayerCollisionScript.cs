a3d3e83160c1ffa168a4cade8587fce7e29e52b8
calumbrown1 <calumbrown1@Hotmail.com>
2016-10-05 18:04:05 +0100

    Renamed CollisionScript to PlayerCollisionScript
    See Commit Name

--- WierdPig/Assets/Scripts/PlayerCollisionScript.cs
+++ WierdPig/Assets/Scripts/PlayerCollisionScript.cs
@@ -5,7 +5,7 @@ using System.Collections;
 /// Collision detector script for player
 /// Sends collision info to PlayerScript
 /// </summary>
-public class CollisionScript : MonoBehaviour {
+public class PlayerCollisionScript : MonoBehaviour {
 
     //Reference to playerscript attatched to player gameobject 
     PlayerScript playerScript;
@@ -23,6 +23,11 @@ public class CollisionScript : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D other)
     {
         //Call collision handelling method since more functionality is availiable outside this method
+        playerScript.HandleCollisions(other.gameObject.tag.ToString());
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        //Call collision handelling method in playerScript
         playerScript.HandleCollisions(other.gameObject.tag.ToString());
     }
 }
